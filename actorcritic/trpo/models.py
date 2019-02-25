dc0059cc068d88e0939446f1f45589b8a5f2ca8c
Yangcy <13520183356@139.com>
2019-02-25 16:55:49 +0800

    changed trpomodels

--- actorcritic/trpo/models.py
+++ actorcritic/trpo/models.py
@@ -6,10 +6,11 @@ import torch.nn as nn
 class Policy(nn.Module):
     def __init__(self, num_inputs, num_outputs):
         super(Policy, self).__init__()
-        self.affine1 = nn.Linear(num_inputs, 256)
-        self.affine2 = nn.Linear(256, 256)
+        self.affine1 = nn.Linear(num_inputs, 2048)
+        self.affine2 = nn.Linear(2048, 256)
+        self.affine3 = nn.Linear(256, 32)
 
-        self.action_mean = nn.Linear(256, num_outputs)
+        self.action_mean = nn.Linear(32, num_outputs)
         self.action_mean.weight.data.mul_(0.1)
         self.action_mean.bias.data.mul_(0.0)
 
@@ -22,7 +23,7 @@ class Policy(nn.Module):
     def forward(self, x):
         x = torch.tanh(self.affine1(x))
         x = torch.tanh(self.affine2(x))
-
+        x = torch.tanh(self.affine3(x))
         action_mean = self.action_mean(x)
         action_log_std = self.action_log_std.expand_as(action_mean)
         action_std = torch.exp(action_log_std)
@@ -33,8 +34,9 @@ class Policy(nn.Module):
 class Value(nn.Module):
     def __init__(self, num_inputs):
         super(Value, self).__init__()
-        self.affine1 = nn.Linear(num_inputs, 256)
-        self.affine2 = nn.Linear(256, 256)
+        self.affine1 = nn.Linear(num_inputs, 2048)
+        self.affine2 = nn.Linear(2048, 256)
+        self.affine3 = nn.Linear(256, 256)
         self.value_head = nn.Linear(256, 1)
         self.value_head.weight.data.mul_(0.1)
         self.value_head.bias.data.mul_(0.0)
@@ -42,6 +44,6 @@ class Value(nn.Module):
     def forward(self, x):
         x = torch.tanh(self.affine1(x))
         x = torch.tanh(self.affine2(x))
-
+        x = torch.tanh(self.affine3(x))
         state_values = self.value_head(x)
         return state_values
